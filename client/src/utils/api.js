5fffc9a597e08cffc2e051ea4b3e535df1d3fbb9
karans18 <karannikumbh18@gmail.com>
2026-04-22 17:51:23 +0530

    Frontend Deployment fix

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-//DEPLOY:
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL || '/api',
-// });
-
+DEPLOY:
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api' 
+    baseURL: import.meta.env.VITE_API_URL || '/api',
 });
+
+// const api = axios.create({
+//     baseURL: 'http://localhost:5000/api' 
+// });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
